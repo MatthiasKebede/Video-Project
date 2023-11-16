e68f015f13494d3aa598e65646178e3575061fb6
MatthiasKebede <msk9862@nyu.edu>
2023-11-16 16:46:39 +0400

    11/16 end of class

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 console.log("check");
 
+
+
 // // HTML Elements
+let currentvid = document.getElementById("vid1");
+
+
+
+
+// // Event Listeners
+
+
+
+
+// // Functions
+// Play the video (or pause if already playing)
+function playVid() {
+    if (currentvid.paused) {
+        currentvid.play();
+    }
+    else {
+        currentvid.pause();
+    }
+}
